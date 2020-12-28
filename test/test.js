93655ab5c609509ee5b0f4215d81742c08e399ab
Tobias Herber <tobias@herber.space>
2020-12-28 15:02:29 +0100

    Make sure test works with uniqe id

--- test/test.js
+++ test/test.js
@@ -2,19 +2,27 @@ const avatar = require('../index');
 
 test('generates avatar', () => {
   const a = avatar('hello world');
-  
-  expect(a).toEqual(`<?xml version="1.0" encoding="UTF-8"?>
+
+  const start = `<?xml version="1.0" encoding="UTF-8"?>
 <svg  viewBox="0 0 80 80" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
-    <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="g">
+    <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="`;
+
+    const mid = `">
       <stop stop-color="rgb(255, 123, 0)" offset="0%"></stop>
       <stop stop-color="rgb(0, 255, 123)" offset="100%"></stop>
     </linearGradient>
   </defs>
-  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-    <rect id="Rectangle" fill="url(#g)" x="0" y="0" width="80" height="80"></rect>
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <rect id="Rectangle" fill="url(#`;
+
+  const end = `)" x="0" y="0" width="80" height="80"></rect>
   </g>
-</svg>`);
+</svg>`;
+
+  expect(a.startsWith(start)).toBeTruthy();
+  expect(a.endsWith(end)).toBeTruthy();
+  expect(a.includes(mid)).toBeTruthy();
 });
 
 test('generates avatar with size', () => {
